dcfa6a8cb5443f530e05ced0ca85f870730bfe64
V Mendez <victormendez990@gmail.com>
2022-10-07 03:22:52 -0600

    Add geocode function for city name

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,6 +1,9 @@
 // Bring in the search form
 var searchFormEl = document.querySelector('.search-bar');
 
+// Define variables
+var searchedCities = []
+
 // Function for form submit
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -13,7 +16,9 @@ function handleFormSubmit(event) {
         return;
     }
 
-    searchApi(searchInput)
+
+
+    geoCode(searchInput);
 }
 
 // Event Listener for search bar
@@ -22,9 +27,30 @@ searchFormEl.addEventListener('submit', handleFormSubmit)
 // Function to search 
 function searchApi(query) {
     console.log(query);
+
+    var locQueryUrl = 'api.openweathermap.org/data/2.5/forecast';
+
+    locQueryUrl = locQueryUrl + '?lat=' + lat + '&lon=' + lon + '&appid=' + key
 }
 
 // Function to print results from search
 function printResults(resultObj) {
     
+}
+
+// Function to geocode from city input
+function geoCodeApi(searchInput) {
+
+    
+    // Variable to store city info once retrieved from API call
+    var city = {
+        name : searchInput,
+        lat : lat,
+        lon : lon,
+    }
+
+    // Save city to local storage for future use
+    searchedCities.push(city);
+    searchedCities = searchedCities.concat(JSON.parse(localStorage.getItem("searchedCities")|| '[]'));
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
 }
